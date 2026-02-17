1ce688534fe464a2f24015b2a5cda9ab8a1c48cc
Goood123-boop <hdiaz@asbarcelona.com>
2026-02-17 14:58:46 +0000

    added about and tasks, with links

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/task")
+def tasks():
+    return render_template("task.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
